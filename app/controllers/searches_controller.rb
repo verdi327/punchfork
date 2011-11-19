89f8fd11eeba697e5757cfa28a37b842e4dde5e9
Brian Mehrman <bcmehrman@gmail.com>
2011-11-19 17:18:14 -0600

    added dynamic search by replacing spaces with '+'

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,7 +8,12 @@ class SearchesController < ApplicationController
     require 'uri'
     require 'net/https'
     require 'json'
-    uri = URI.parse("http://api.punchfork.com/recipes?key=269e5381c4164515&q=chicken+parmesan&count=2")
+    
+    search_str = params[:search]
+    
+    query = search_str.gsub(/\s/,'+')
+    
+    uri = URI.parse("http://api.punchfork.com/recipes?key=269e5381c4164515&q=#{query}&count=10")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     
