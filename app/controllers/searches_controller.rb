19f63a034e5fffc06f23245eefbeaaaef0cb3ac2
Brian Mehrman <bcmehrman@gmail.com>
2011-11-19 17:08:56 -0600

    parsed the JSON into a hash

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,12 +7,14 @@ class SearchesController < ApplicationController
   def create
     require 'uri'
     require 'net/https'
+    require 'json'
     uri = URI.parse("http://api.punchfork.com/recipes?key=269e5381c4164515&q=chicken+parmesan&count=2")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     
     @response = http.request(request)
-    
+    @output = JSON.parse(@response.body)
+    @recipes = @output["recipes"]
     
   end
   
