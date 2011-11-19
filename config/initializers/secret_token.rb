931927eb7a823dd50a3ca5b100e8164cbdf4d163
Michael <michael.v.verdi@gmail.com>
2011-11-19 13:51:56 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Punchfork::Application.config.secret_token = 'ab1bdf5968e048ee38220a9279c7c65c6c0ab1e8ad5b093115fbf22ead7f3473b000786c3dbcffff25f088cce8a1ab38cd5ce38c4d011a7aaeeffaa9403e68a6'
